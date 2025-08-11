38f675f953a769d01bc90086b74749e91b8da587
hackhud <113374031+hackhud@users.noreply.github.com>
2025-08-12 01:33:16 +0300

    DreamDrop v1.02 realise. - Added new command functional (/dreamdrop give, /dreamdrop get) - Upgraded structure of DreamDropCommand codebase

--- src/ua/hackhud/DreamDrop/Listener/DreamDropCommand.java
+++ src/ua/hackhud/DreamDrop/Listener/DreamDropCommand.java
@@ -37,6 +37,9 @@ public class DreamDropCommand implements CommandExecutor {
             case "add":
                 handleAdd(sender, args);
                 break;
+            case "give":
+                handleGive(sender, args);
+                break;
             default:
                 MessageUtils.sendMessage(sender, "&c>> DreamDrop: Неизвестная команда.");
         }
@@ -61,25 +64,50 @@ public class DreamDropCommand implements CommandExecutor {
     private void sendUsage(CommandSender sender) {
         MessageUtils.sendMessage(sender, "&e>> DreamDrop: &7Доступные команды:");
         MessageUtils.sendMessage(sender, "&7/dreamdrop reload - Перезагрузить конфигурацию");
-        MessageUtils.sendMessage(sender, "&7/dreamdrop get <storageName> - Получить случайный предмет");
-        MessageUtils.sendMessage(sender, "&7/dreamdrop add simple <storageName> - Добавить предмет в простое хранилище");
-        MessageUtils.sendMessage(sender, "&7/dreamdrop add weighted <storageName> <weight> - Добавить предмет в взвешенное хранилище");
+        MessageUtils.sendMessage(sender, "&7/dreamdrop get <weightedStorage> - Получить случайный предмет из взвешенного хранилища");
+        MessageUtils.sendMessage(sender, "&7/dreamdrop get <simpleStorage> <id> - Получить предмет по айди из простого хранилища");
+        MessageUtils.sendMessage(sender, "&7/dreamdrop add simple <simpleStorage> - Добавить предмет в простое хранилище");
+        MessageUtils.sendMessage(sender, "&7/dreamdrop add weighted <weightedstorage> <weight> - Добавить предмет в взвешенное хранилище");
+        MessageUtils.sendMessage(sender, "&7/dreamdrop give <player> <weightedStorage> - Выдать игроку случайный предмет из взвешенного хранилища");
+        MessageUtils.sendMessage(sender, "&7/dreamdrop give <player> <simpleStorage> <id> - Выдать игроку предмет по айди из простого хранилища");
     }
 
     private void handleReload(CommandSender sender) {
         Main.getPlugin().getConfigManager().reload();
         Main.getPlugin().getStorageManager().reload();
-        MessageUtils.sendMessage(sender, "&a>> DreamDrop: Плагин успешно перезагружен!");
+        MessageUtils.sendMessage(sender, "&e>> DreamDrop: Плагин успешно перезагружен!");
     }
 
     private void handleGet(CommandSender sender, String[] args) {
         if (args.length < 2) {
-            MessageUtils.sendMessage(sender, "&c>> DreamDrop: &7Использование: /dreamdrop get <storageName>");
+            MessageUtils.sendMessage(sender, "&c>> DreamDrop: &7Использование: /dreamdrop get <weightedStorage>");
+            MessageUtils.sendMessage(sender, "&c>> DreamDrop: &7Использование: /dreamdrop get <simpleStorage> <id>");
             return;
         }
 
         Player player = (Player) sender;
         String storageName = args[1];
+
+        if (args.length == 3) {
+            if (!storageManager.isSimpleStorageExists(storageName)) {
+                MessageUtils.sendMessage(sender, "&c>> DreamDrop: Обычного хранилища с названием '" + storageName + "' не существует!");
+                return;
+            }
+
+            try {
+                int simpleStorageItemId = Integer.parseInt(args[3]);
+                ItemStack item = storageManager.getSimpleStorage(storageName).getItem(simpleStorageItemId);
+                if (item == null) {
+                    MessageUtils.sendMessage(sender, "&c>> DreamDrop: В хранилище '" + storageName + "' нет предмета с айди '" + simpleStorageItemId + "'!");
+                }
+
+                player.getInventory().addItem(item);
+                MessageUtils.sendMessage(sender, "&e>> DreamDrop: Вы получили предмет из хранилища '" + storageName + "' с айди '" + simpleStorageItemId + "'!");
+            } catch (NumberFormatException e) {
+                MessageUtils.sendMessage(sender, "&c>> DreamDrop: &7Айди должен быть целым числом!");
+            }
+            return;
+        }
 
         if (!storageManager.isWeightedStorageExists(storageName)) {
             MessageUtils.sendMessage(sender, "&c>> DreamDrop: Хранилище '" + storageName + "' не найдено!");
@@ -93,7 +121,7 @@ public class DreamDropCommand implements CommandExecutor {
         }
 
         player.getInventory().addItem(item);
-        MessageUtils.sendMessage(sender, "&a>> DreamDrop: Вы получили предмет из хранилища '" + storageName + "'");
+        MessageUtils.sendMessage(sender, "&e>> DreamDrop: Вы получили предмет из хранилища '" + storageName + "'");
     }
 
     private void handleAdd(CommandSender sender, String[] args) {
@@ -125,6 +153,59 @@ public class DreamDropCommand implements CommandExecutor {
         }
     }
 
+    private void handleGive(CommandSender sender, String[] args) {
+        if (args.length < 3) {
+            MessageUtils.sendMessage(sender, "&c>> DreamDrop: &7Использование: /dreamdrop give <player> <weightedStorage>");
+            MessageUtils.sendMessage(sender, "&c>> DreamDrop: &7Использование: /dreamdrop give <player> <simpleStorage> <id>");
+            return;
+        }
+
+        Player target = Main.getPlugin().getServer().getPlayer(args[1]);
+        if (target == null || !target.isOnline()) {
+            MessageUtils.sendMessage(sender, "&c>> DreamDrop: Игрок '" + args[1] + "' не найден или не онлайн!");
+            return;
+        }
+
+        String storageName = args[2];
+
+        if (args.length == 4) {
+            if (!storageManager.isSimpleStorageExists(storageName)) {
+                MessageUtils.sendMessage(sender, "&c>> DreamDrop: Обычного хранилища с названием '" + storageName + "' не существует!");
+                return;
+            }
+
+            try {
+                int simpleStorageItemId = Integer.parseInt(args[3]);
+                ItemStack item = storageManager.getSimpleStorage(storageName).getItem(simpleStorageItemId);
+                if (item == null) {
+                    MessageUtils.sendMessage(sender, "&c>> DreamDrop: В хранилище '" + storageName + "' нет предмета с айди '" + simpleStorageItemId + "'!");
+                    return;
+                }
+                target.getInventory().addItem(item);
+                MessageUtils.sendMessage(target, "&e>> DreamDrop: Вы получили предмет из хранилища '" + storageName + "' с айди '" + simpleStorageItemId + "'!");
+                MessageUtils.sendMessage(sender, "&e>> DreamDrop: Предмет выдан игроку '" + target.getName() + "' из хранилища '" + storageName + "' с айди '" + simpleStorageItemId + "'!");
+            } catch (NumberFormatException e) {
+                MessageUtils.sendMessage(sender, "&c>> DreamDrop: &7Айди должен быть целым числом!");
+            }
+            return;
+        }
+
+        if (!storageManager.isWeightedStorageExists(storageName)) {
+            MessageUtils.sendMessage(sender, "&c>> DreamDrop: Взвешенного хранилища '" + storageName + "' не найдено!");
+            return;
+        }
+
+        ItemStack item = storageManager.getWeightedStorage(storageName).getRandomItem();
+        if (item == null) {
+            MessageUtils.sendMessage(sender, "&c>> DreamDrop: В хранилище '" + storageName + "' нет предметов!");
+            return;
+        }
+
+        target.getInventory().addItem(item);
+        MessageUtils.sendMessage(target, "&e>> DreamDrop: Вы получили предмет из хранилища '" + storageName + "'");
+        MessageUtils.sendMessage(sender, "&e>> DreamDrop: Предмет выдан игроку '" + target.getName() + "' из хранилища '" + storageName + "'");
+    }
+
     private void handleSimpleStorageAdd(CommandSender sender, String storageName, ItemStack item) {
         SimpleStorage storage = storageManager.getSimpleStorage(storageName);
         storage.addItem(item.clone());
@@ -133,7 +214,7 @@ public class DreamDropCommand implements CommandExecutor {
 
     private void handleWeightedStorageAdd(CommandSender sender, String[] args, String storageName, ItemStack item) {
         if (args.length < 4) {
-            MessageUtils.sendMessage(sender, "&c>> DreamDrop: &7Использование: /dreamdrop add weighted <storageName> <weight>");
+            MessageUtils.sendMessage(sender, "&c>> DreamDrop: &7Использование: /dreamdrop add weighted <storage> <weight>");
             return;
         }
 
